13cc8a764d243bd09e349a2adb50f0eb9d7a7a40
hugojm <hjimenez.upc@gmail.com>
2021-04-06 17:50:34 +0200

    flask

---
+++ display.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image, ImageTk
+import os
+import torch
+from torchvision import transforms
+from matplotlib import pyplot as plt
+import matplotlib
+from matplotlib.figure import Figure
+import numpy as np
+
+def print_mask(filename):
+    matplotlib.use('agg')
+    device = 'cpu'
+    model = torch.load('static/weight/weights.pt' , map_location = torch.device('cpu'))
+    img = Image.open(filename)
+    data_transforms = transforms.Compose([transforms.ToTensor()])
+    data_img = data_transforms(img)
+    inputs = data_img.unsqueeze(0).to(device)
+    outputs = model(inputs)
+    plt.imshow(torch.split(data_img.unsqueeze(0),1,0)[0].squeeze().permute(1, 2, 0))
+    cmaps = ['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds']
+    plt.imshow(torch.split(data_img.unsqueeze(0),1,0)[0].squeeze().permute(1, 2, 0), alpha=0.7)
+    for i in range(6):
+        mask = torch.split(outputs['out'].cpu(),1,0)[0].squeeze()[i].detach().numpy()
+        mask = np.ma.masked_where(mask < 0.1, mask)
+        plt.imshow(mask, cmaps[i], alpha = 0.7)
+    plt.savefig('uploads/out.png')
--- run.py
+++ run.py
@@ -1,8 +1,76 @@
+#!/usr/bin/python3
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from PIL import Image, ImageTk
+import os
+import torch
+from torchvision import transforms
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+import numpy as np
+from display import print_mask
 
-from flask import Flask
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-posts = []
-@app.route("/")
+
+
+@app.route('/')
 def index():
-    return "{} posts".format(len(posts))
+    return render_template('index.html')
+
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/terato.html', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print_mask(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                     filename='out.png'))
+    return render_template('terato.html')
+
+@app.route('/cardio.html')
+def cardio():
+    return render_template('cardio.html')
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)

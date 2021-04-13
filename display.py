be03408bdd3e50796cbb85d2b81421fa1d295d8b
hugojm <hjimenez.upc@gmail.com>
2021-04-13 08:15:35 +0200

    folder not working

--- display.py
+++ display.py
@@ -8,20 +8,36 @@ import matplotlib
 from matplotlib.figure import Figure
 import numpy as np
 
+
+def name(index):
+    if (index == 0):
+        return "eyes_dorsal"
+    elif (index == 1):
+        return "outline_lateral"
+    elif (index == 2):
+        return "yolk"
+    elif (index == 3):
+        return "heart"
+    elif (index == 4):
+        return "outline_dorsal"
+    else:
+        return "ov"
+    return
+
 def print_mask(filename):
     matplotlib.use('agg')
     device = 'cpu'
     model = torch.load('static/weight/weights.pt' , map_location = torch.device('cpu'))
-    img = Image.open(filename)
+    img = Image.open('/Users/hugo.jimenez/Desktop/20190902_1046_R1_1056_R1_tail_W_right_G02_1_2.png')
     data_transforms = transforms.Compose([transforms.ToTensor()])
     data_img = data_transforms(img)
     inputs = data_img.unsqueeze(0).to(device)
     outputs = model(inputs)
-    plt.imshow(torch.split(data_img.unsqueeze(0),1,0)[0].squeeze().permute(1, 2, 0))
     cmaps = ['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds']
-    plt.imshow(torch.split(data_img.unsqueeze(0),1,0)[0].squeeze().permute(1, 2, 0), alpha=0.7)
     for i in range(6):
         mask = torch.split(outputs['out'].cpu(),1,0)[0].squeeze()[i].detach().numpy()
         mask = np.ma.masked_where(mask < 0.1, mask)
+        plt.imshow(torch.split(data_img.unsqueeze(0),1,0)[0].squeeze().permute(1, 2, 0), alpha=0.7)
         plt.imshow(mask, cmaps[i], alpha = 0.7)
-    plt.savefig(filename[:-4]+'_out.png', bbox_inches='tight')
+        part = name(i)
+        plt.savefig(filename[:-4]+'_'+ part +'_out.png', bbox_inches='tight')
--- run.py
+++ run.py
@@ -13,7 +13,7 @@ import numpy as np
 from display import print_mask
 
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
@@ -40,21 +40,26 @@ def allowed_file(filename):
 
 @app.route('/terato.html', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print_mask(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    # if request.method == 'POST':
+    #     # check if the post request has the file part
+    #     if 'file' not in request.files:
+    #         flash('No file part')
+    #         return redirect(request.url)
+    #     file = request.files['file[]']
+    #     # if user does not select file, browser also
+    #     # submit an empty part without filename
+    #     if file.filename == '':
+    #         flash('No selected file')
+    #         return redirect(request.url)
+    #     if file and allowed_file(file.filename):
+    #         filename = secure_filename(file.filename)
+    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    if request.method == "POST":
+        files = request.files.getlist("file[]")
+        for file in files:
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            print(file.filename)
             return render_template('terato.html' , input = filename , output = filename[:-4]+'_out.png', file=True)
     return render_template('terato.html', file=False)
 

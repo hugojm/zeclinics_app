7ae89e76dc15fce34b40bd6d9a0af7b19c671ef8
hugojm <hjimenez.upc@gmail.com>
2021-05-18 08:42:19 +0200

    working

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ import pickle
 import time
 import random
 import threading
-
+from flaskwebgui import FlaskUI  # import FlaskUI
 
 UPLOAD_FOLDER = 'static/images'
 UPLOAD_FOLDER_CARDIO = 'static/videos'
@@ -33,8 +33,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['UPLOAD_FOLDER_CARDIO'] = UPLOAD_FOLDER_CARDIO
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-
-
+ui = FlaskUI(app, width=3000, height=3000, maximized=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -48,6 +47,7 @@ def index():
         return redirect('upload_cardio.html')
     return render_template('index.html')
 
+
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     print("Youre in home")
@@ -58,6 +58,7 @@ def home():
             return redirect('upload.html')
         return redirect('upload_cardio.html')
     return render_template('index.html')
+
 
 @app.route('/upload_cardio.html', methods=['GET', 'POST'])
 def upload_cardio():
@@ -77,9 +78,11 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def allowed_file_cardio(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_CARDIO
+
 
 @app.route('/upload.html', methods=['GET', 'POST'])
 def upload_file():
@@ -89,37 +92,39 @@ def upload_file():
             if file and allowed_file(file.filename):
                 path_name = Path(file.filename)
                 if (not Path(os.path.join(app.config['UPLOAD_FOLDER'], path_name.parent)).exists()):
-                    os.makedirs(os.path.join(app.config['UPLOAD_FOLDER'], path_name.parent))
+                    os.makedirs(os.path.join(
+                        app.config['UPLOAD_FOLDER'], path_name.parent))
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], path_name))
         plate_name = Path(files[0].filename).parent.parent
         if plate_name == Path('.'):
             plate_name = Path(files[0].filename).parent
-        plate(app.config['UPLOAD_FOLDER'],str(plate_name))
+        plate(app.config['UPLOAD_FOLDER'], str(plate_name))
         dirname = os.path.join(app.config['UPLOAD_FOLDER'], plate_name)
-        dirs = [ name for name in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, name)) ]
-        dirs2 = [dirname+ "/" + sub for sub in dirs]
+        dirs = [name for name in os.listdir(
+            dirname) if os.path.isdir(os.path.join(dirname, name))]
+        dirs2 = [dirname + "/" + sub for sub in dirs]
         dirs2.sort()
         with open('static/dict/' + str(plate_name) + '.pckl', 'rb') as handle:
             booleans = pickle.load(handle)
-        return render_template('terato2.html', plates = dirs2, done=True, dict = booleans)
+        return render_template('terato2.html', plates=dirs2, done=True, dict=booleans)
     processed = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('upload.html', process = processed)
+    return render_template('upload.html', process=processed)
+
 
 @app.route('/terato', methods=['GET', 'POST'])
 def terato():
     if request.method == 'POST':
         plate_name = request.form['submit_button']
         dirname = os.path.join(app.config['UPLOAD_FOLDER'], plate_name)
-        dirs = [ name for name in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, name)) ]
-        dirs2 = [dirname+ "/" + sub for sub in dirs]
+        dirs = [name for name in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, name))]
+        dirs2 = [dirname + "/" + sub for sub in dirs]
         dirs2.sort()
         with open('static/dict/' + str(plate_name) + '.pckl', 'rb') as handle:
             booleans = pickle.load(handle)
-        return render_template('terato2.html', plates = dirs2, dict = booleans)
+        return render_template('terato2.html', plates = dirs2, dict=booleans)
     else:
         print("fail")
-
 
 
 '''
@@ -140,6 +145,7 @@ def terato():
     return render_template('upload.html')
 '''
 
+
 @app.route('/cardio.html', methods=['GET', 'POST'])
 def cardio():
     if request.method == "POST":
@@ -147,29 +153,36 @@ def cardio():
         for file in files:
             if file and allowed_file_cardio(file.filename):
                 path_name = Path(file.filename)
-                path_name = os.path.join('./' , path_name)
+                path_name = os.path.join('./', path_name)
                 matplotlib.use('agg')
                 # masks_a, masks_v , a , v, _ = process_video(path_name,update_it=4, skip=1, debug=True, gen_video=False, video_name=os.path.join(app.config['UPLOAD_FOLDER_CARDIO'],'out.webm'),mode="ac", p_out_shape="original")
                 # hp.plotter(a[1],a[2],show=False, title='Atrium signal').savefig(os.path.join(app.config['UPLOAD_FOLDER_CARDIO'],'out1.png'))
                 # hp.plotter(v[1],v[2],show=False, title='Ventricle signal').savefig(os.path.join(app.config['UPLOAD_FOLDER_CARDIO'],'out2.png'))
         with open('static/dict/metrics.pckl', 'rb') as handle:
             metrics = pickle.load(handle)
-        return render_template('cardio.html', print=True, dict = metrics)
+        return render_template('cardio.html', print=True, dict=metrics)
     else:
         with open('static/dict/metrics.pckl', 'rb') as handle:
             metrics = pickle.load(handle)
-        return render_template('cardio.html', print=True, dict = metrics)
+        return render_template('cardio.html', print=True, dict=metrics)
     return render_template('cardio.html', dict={})
+
 
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
+
 
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
         filename = values.get('filename', None)
         if filename:
             file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
+                                     endpoint, filename)
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
+
+
+if __name__ == "__main__":
+    # app.run() for debug
+    ui.run()

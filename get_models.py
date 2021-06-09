477fc9eee59fb22f66a62bbc0fd3c6fc50bafc9c
megaelius <abad.elias00@gmail.com>
2021-06-09 18:31:16 +0200

    error in paths

--- get_models.py
+++ get_models.py
@@ -1,8 +1,9 @@
 
 import gdown
+import os
 urls=['https://drive.google.com/uc?id=1C9xK9saEEolu3lkn57AY0WlejJoArrGB',
       'https://drive.google.com/uc?id=1VKmsaWw9dTFJlHttoU7ebu5P5i-7dKzn',
       'https://drive.google.com/uc?id=1NaN4F9xOerLsWNGV2DZFw21k0UvqdA3n']
-output = './zeclinics_app/static/weight/'
+output = 'zeclinics_app'+os.sep+'static'+os.sep+'weight'+os.sep
 for url in urls:
     gdown.download(url,output,quiet=False)
